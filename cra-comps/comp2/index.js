831ecf79064652d52b7c11f2275e77fb1bfd7b98
Bradford Lemley <bradfordlemley@gmail.com>
2018-01-10 18:19:21 -0700

    Clean up; automatically lerna bootstrap on npm install.

--- cra-comps/comp2/index.js
+++ cra-comps/comp2/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 import {Add} from 'comp1';
 
-import answer from '../oops';
-
 const Comp2 = ({a, b}) =>
   <div>
     Comp2: <Add a={a} b={b} />
-    Answer: {answer}
   </div>
 
 export default Comp2;
